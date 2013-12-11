eb68a0f6e23432ccb03ce912d4db5c2af991826c
alawadh <alawadh@colorado.edu>
2013-12-10 17:26:48 -0800

    fixed login

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,22 @@
+class SessionsController < ApplicationController
+  
+  def new
+  end
+
+  def create
+	user = User.authenticate(params[:username], params[:password])
+	if user
+		session[:user_id] = user.id
+		redirect_to user_path(user), :notice => "Logged in!"
+	else
+		flash.now.alert = "Invalid username or password"
+		render "new"
+	end
+  end
+
+  def destroy
+	session[:user_id] = nil
+	redirect_to log_in_path, :notice => "Logged out!"
+  end
+
+end
